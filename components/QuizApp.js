b3d1c1d3e2fdf87138ba9cd77c9eea9383c4f4a1
mokpi <34705943+mokpi@users.noreply.github.com>
2021-07-04 01:53:08 +0900

    Remove unneeded.

--- components/QuizApp.js
+++ components/QuizApp.js
@@ -181,56 +181,5 @@ export default function QuizApp(props) {
         />
       ) : <Congratulations/> : <LoadingLayout/>}
     </div>
-    /*
-    <div className="h-screen app-container">
-      <nav className="flex py-16 items-center">
-        <div className="flex-grow"></div>
-        <div
-          style={{
-            opacity: loaded ? 1 : 0,
-          }}
-          className={styles.progressBar}
-        >
-          <div
-            style={{
-              width: progress * 100 + "%",
-            }}
-            className={styles.progress}
-          />
-        </div>
-        <div
-          className="flex-grow"
-          style={{
-            height: 20, // for svg icon to get same height as progress bar
-            paddingLeft: 40
-          }}
-        >
-          <Link href="/">
-            <a><CrossIcon/></a>
-          </Link>
-        </div>
-      </nav>
-      {showLogin ? <Login/> : loaded ? pageNum < challenges.length ? (
-        <Challenge
-          challenge={challenges[pageNum]}
-          isLastQuestion={pageNum === numPages - 1}
-          onCorrect={() => {
-            setProgress((progress) => progress + 1 / numPages);
-          }}
-          onNext={(isCorrect) => {
-            if (isCorrect)
-              setPageNum((pageNum) => pageNum + 1);
-            else
-              setChallenges((challenges) => {
-                // slice the incorrect question out to the end of list,
-                // and do not increment page number
-                const spliced_challenges = challenges.splice(pageNum, 1); // mutates challenges
-                return [...challenges, ...spliced_challenges];
-              });
-          }}
-        />
-      ) : <Congratulations/> : <LoadingLayout/>}
-    </div>
-    */
   );
 }
