597a4a6aee9dfe55944f60f05b21552cc4e5d7bf
mokpi <34705943+mokpi@users.noreply.github.com>
2021-07-12 00:26:28 +0900

    Admin: check missing word

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -4,5 +4,6 @@ export default function AdminHome() {
     return <ul>
         <li><Link href="problems_table"><a>Go to word list</a></Link></li>
         <li><Link href="two_way_reference"><a>Go to 2 way reference generator</a></Link></li>
+        <li><Link href="mc_translate_missing_word"><a>Go to mc_translate_missing_word</a></Link></li>
     </ul>;
 }
---
+++ pages/admin/mc_translate_missing_word.js
@@ -0,0 +1,99 @@
+import firebase from '@/common/firebase_init'
+import 'firebase/firestore'
+
+import { useEffect, useMemo, useState } from 'react';
+
+const db = firebase.firestore();
+
+export default function ProblemsTable() {
+  const [problems, setProblems] = useState([]);
+  const [topics, setTopics] = useState([]);
+
+  const rows = useMemo(() => {
+    const rows_ = [...problems];
+
+    // for bettwe performance, use hashmap instead.
+    for (let j = 0; j < rows_.length; j++) {
+      rows_[j]._belongTopic = -1;
+      if (!rows_[j].topic) continue;
+      for (let i = 0; i < topics.length; i++) {
+        if (rows_[j].topic.isEqual(topics[i]._ref))
+          rows_[j]._belongTopic = i;
+      }
+    }
+
+    return rows_;
+  }, [problems, topics]);
+
+  useEffect(() => {
+    const unsubArray = [
+      db.collection("problems").onSnapshot(snap => {
+        setProblems(snap.docs
+          .map(doc => ({...doc.data(), id: doc.id, _ref: doc.ref}))
+          .filter(doc => doc.type==='mc' && doc.subtype==='translate')
+        )
+      }),
+      db.collection("topic").onSnapshot(snap => {
+        setTopics(snap.docs.map(doc => ({...doc.data(), id: doc.id, _ref: doc.ref})))
+      }),
+    ];
+
+    return () => {
+      for (const unsub of unsubArray)
+        unsub();
+    }
+  }, [])
+
+  function changeTopicHandler(row) {
+    return (e) => {
+      const i = parseInt(e.target.value);
+      if (i >= 0 && i < topics.length) {
+        Promise.all ([
+          // remove from old topic
+          row.topic?.update({
+            problems: firebase.firestore.FieldValue.arrayRemove(row._ref)
+          }),
+
+          // add to new topic
+          topics[i]._ref.update({
+            problems: firebase.firestore.FieldValue.arrayUnion(row._ref)
+          }),
+
+          // update topic
+          row._ref.update({
+            topic: topics[i]._ref
+          }),
+        ]).then(() => {
+          console.log("Done Updating!!");
+        }).catch(() => {
+          console.error("Error occurred during update.");
+        })
+      }
+    }
+  }
+  
+
+  return <div>
+    <h1>Problems Details</h1>
+    <table className="border-collapse">
+      <thead>
+        <tr>
+            <th>Document ID</th>
+            <th>Details</th>
+            <th>Topic</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map((row, i) => {
+          return (
+            <tr key={row.id}>
+              <td className="border">{row.id}</td>
+              <td className="border"><pre>{row.answer}</pre></td>
+              <td className="border">{row.sentence ? "OK" : "Oh my god!"}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  </div>;
+}
