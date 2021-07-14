b8350714c934ad7e3ed5fb11ca3f886bdd458399
mokpi <34705943+mokpi@users.noreply.github.com>
2021-07-15 01:14:15 +0900

    Simplify resizing code.

--- components/Problems/AnswerArea/AutoSizeButton.tsx
+++ components/Problems/AnswerArea/AutoSizeButton.tsx
@@ -7,21 +7,19 @@ export default function AutoSizeButton(props: React.ButtonHTMLAttributes<HTMLBut
   const inner = useRef<HTMLSpanElement>();
   const fontRatio = useRef(1.);
 
-  const rawResize = useCallback((newRatio) => {
-    fontRatio.current = newRatio;
-    inner.current.style.fontSize = fontRatio.current + "em";
+  const doResize = useCallback((outterWidth: number) => {
+    if (!inner.current) return;
+
+    const newRatio = Math.max(Math.min(fontRatio.current * outterWidth / inner.current.clientWidth, 1.0), 0.1);
+
+    if (fontRatio.current != newRatio) {
+      fontRatio.current = newRatio;
+      inner.current.style.fontSize = fontRatio.current + "em";
+    }
   }, [])
 
   useEffect(() => {
     const outterEl = outter.current;
-    function doResize(outterWidth: number) {
-      if (!inner.current) return;
-
-      const newRatio = Math.max(Math.min(fontRatio.current * outterWidth / inner.current.clientWidth, 1.0), 0.1);
-
-      if (fontRatio.current != newRatio)
-        rawResize(newRatio);
-    }
     const resizeObserver = new ResizeObserver(entries => {
       for (let entry of entries) {
         if (entry.contentBoxSize[0]) {
@@ -38,12 +36,11 @@ export default function AutoSizeButton(props: React.ButtonHTMLAttributes<HTMLBut
       resizeObserver.unobserve(outterEl)
       resizeObserver.disconnect()
     }
-  }, [rawResize]);
+  }, [doResize]);
 
   useEffect(() => {
-    if (fontRatio.current != 1.0)
-      rawResize(1.0);
-  }, [props.children, rawResize]);
+    doResize(outter.current.clientWidth)
+  }, [doResize, props.children]);
 
   return <button {...props}>
     <span className={styles.outterSpan} ref={outter}>
